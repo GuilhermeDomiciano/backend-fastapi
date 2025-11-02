9ee26de26660dccf63a2f5248c6d11716c9812dc
Domiciano <guilhermedomicianosilva@gmail.com>
2025-11-02 14:12:34 -0300

    update env

--- database.py
+++ database.py
@@ -1,6 +1,7 @@
 from sqlmodel import create_engine
 
-DATABASE_URL = "postgresql+psycopg2://postgres.khaajlvbbipyotipikew:encoinfominicurso2025@aws-1-sa-east-1.pooler.supabase.com:5432/postgres"
+DATABASE_URL = "postgresql+psycopg2://aluno_minicurso:minicurso123@aws-1-sa-east-1.pooler.supabase.com:5432/postgres"
+
 
 engine = create_engine(
     DATABASE_URL,
